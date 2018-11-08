c7cf7aeda79cbb12a02b65cd4bfdcf6902cf7aae
§ <n.bobrov@appyfurious.com>
2018-11-08 23:24:25 +0300

    init

---
+++ Assets/src/MainMenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+	public Button newGame;
+
+	void Start () 
+	{
+		Debug.Log("Menu Start");
+		newGame.onClick.AddListener(onClickNewGame);
+	}
+	
+	void Update () {
+	
+	}
+
+	void onClickNewGame() {
+		Debug.Log("NewGame CLick");
+		Application.LoadLevel("LevelsListScene");
+	}
+}
