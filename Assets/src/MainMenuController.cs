b10737758afba4cf57c9894ec3b09423848452e9
§ <n.bobrov@appyfurious.com>
2018-11-09 10:37:12 +0300

    added buttons in menu

--- Assets/src/MainMenuController.cs
+++ Assets/src/MainMenuController.cs
@@ -8,19 +8,47 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour {
 
 	public Button newGame;
+	public Button inventory;
+	public Button shop;
+	public Button settings;
+	public Button exit;
 
 	void Start () 
 	{
 		Debug.Log("Menu Start");
-		newGame.onClick.AddListener(onClickNewGame);
+		newGame.onClick.AddListener(newGameOnClick);
+		inventory.onClick.AddListener(inventoryOnClick);
+		shop.onClick.AddListener(shopOnClick);
+		settings.onClick.AddListener(settingsOnClick);
+		exit.onClick.AddListener(exitOnClick);
 	}
 	
 	void Update () {
 	
 	}
 
-	void onClickNewGame() {
-		Debug.Log("NewGame CLick");
+	public void newGameOnClick() {
+		Debug.Log("newGameOnClick");
 		Application.LoadLevel("LevelsListScene");
+	}
+
+	public void inventoryOnClick() {
+		Debug.Log("inventoryOnClick");
+		Application.LoadLevel("InventoryScene");
+	}
+
+	public void shopOnClick() {
+		Debug.Log("shopOnClick");
+		Application.LoadLevel("ShopScene");
+	}
+
+	public void settingsOnClick() {
+		Debug.Log("settingsOnClick");
+		Application.LoadLevel("SettingsScene");
+	}
+
+	public void exitOnClick() {
+		Debug.Log("exitOnClick");
+		Application.Quit();
 	}
 }
