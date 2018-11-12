bb3926fa9b2f3d27c84753fcb32ffd4659190163
§ <n.bobrov@appyfurious.com>
2018-11-12 23:59:27 +0300

    added player movement

---
+++ Assets/Scripts/WitchController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchController : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+	private Rigidbody2D rb2D;
+
+	private void Start()
+    {
+		rb2D = GetComponent<Rigidbody2D>();
+    }
+
+	void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+		HandleMovement(horizontal, vertical);
+	}
+
+    private void HandleMovement(float horizontal, float vertical)
+    {
+        rb2D.velocity = new Vector2(horizontal * horizontalSpeed, vertical * verticalSpeed);
+    }
+}
